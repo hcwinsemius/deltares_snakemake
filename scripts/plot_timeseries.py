c4c3b9012fbd632511f08d370f2b2f50b0acafd4
hcwinsemius <h.c.winsemius@gmail.com>
2022-09-13 15:15:14 +0200

    updated RR with better weagen

--- scripts/plot_timeseries.py
+++ scripts/plot_timeseries.py
@@ -1,4 +1,4 @@
-# This first line is used to save the python code below into a file called "src/create_file.py"
+# This first line is used to save the python code below into a file called "scripts/plot_timeseries.py"
 
 ######################################################################################
 ####################### Content of the python script #################################
@@ -6,20 +6,16 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Main function
-def plot_timeseries(
-    csv_files: list, variables: list, outputfile: str, **kwargs
-) -> None:
+# Main function 
+def plot_timeseries(csv_files: list, variables: list, outputfile: str, **kwargs) -> None:
     """
     Reads different csv files in csv_files list and prepares a plot per variable in variables list.
     The plot will be saved to outputfile.
-
+    
     kwargs can be used to pass specific arguments into the pandas.read_csv function.
     """
     # Initialiase plots
-    print(
-        f"Initialising plots for variables {variables} based on csv files: {csv_files}"
-    )
+    print(f"Initialising plots for variables {variables} based on csv files: {csv_files}")
     n = len(variables)
     fig, axes = plt.subplots(n, 1, sharex=True, figsize=(15, n * 4))
     axes = [axes] if n == 1 else axes
@@ -43,25 +39,20 @@ def plot_timeseries(
     for i in range(len(variables)):
         axes[i].set_ylabel(variables[i])
         axes[i].legend()
-
+    
     # Save plot
     print(f"Saving plot in {outputfile}")
     fig.savefig(outputfile, bbox_inches="tight", dpi=200)
-
+    
 
 # Get the snakemake arguments
-csv_files = [
-    "data/precip_evapo_weagen1.csv",
-    "output/river_flow_weagen1.csv",
-    "data/precip_evapo_weagen2.csv",
-    "output/river_flow_weagen2.csv",
-]
-variables = ["p", "ep", "Q"]
-outputfile = "output/plot_results_example.png"
+csv_files = ['data/precip_evapo_weagen1.csv', 'output/river_flow_weagen1.csv', 'data/precip_evapo_weagen2.csv', 'output/river_flow_weagen2.csv']
+variables = ['p', 'ep', 'Q']
+outputfile = 'output/plot_results_example.png'
 
 # Run our function
 plot_timeseries(
-    csv_files=csv_files,
-    variables=variables,
-    outputfile=outputfile,
+    csv_files = csv_files, 
+    variables = variables, 
+    outputfile = outputfile,
 )
--- scripts/weather_generator.py
+++ scripts/weather_generator.py
@@ -1,8 +1,39 @@
 import os
-import hydromt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def get_markov_rainfall(prev_rainfall, p_01, p_10, thres=1., multiplier=20.):
+    """
+    Function to generate daily rainfall from previous day rainfall and markov parameters
+
+    Parameters
+    ----------
+    prev_rainfall : float
+        Amount of rainfall from the previous day in [mm day-1]
+    p_01 : float(0-1)
+        Probability of a wet day following on a dry day
+    p_10 : float(0-1)
+        Probability of a dry day following on a wet day
+    thres : float, optional
+        Threshold defining amount of rainfall [mm day-1] that is considered a dry (lt) or wet (ge) day rainfall amount
+    multiplier : float, optional
+        Multiplier used to convert a randomly drawn number (0-1) into a mm day-1 rainfall amount.
+    Returns
+    -------
+    rainfall : float
+        Amount of rainfall at the present day
+
+    """
+    if prev_rainfall >= thres:
+        wet = np.random.choice(np.array([True, False]), p=np.array([1-p_10, p_10]))
+    else:
+        wet = np.random.choice(np.array([True, False]), p=np.array([p_01, 1-p_01]))
+    if wet:
+        return np.random.rand() * multiplier  # can be under thres, which makes this into a rainfall day that can transition more easily to a dry day.
+    else:
+        return 0.
+
 
 # SnakeMake arguments are automatically passed WITHOUT importing a lib. So this .py file is basicallly used as a template.
 start_date = snakemake.params.start_date
@@ -10,15 +41,31 @@ end_date = snakemake.params.end_date
 fn = os.path.abspath(snakemake.output.weather_csv)
 evapo = snakemake.params.evapo
 multiplier = snakemake.params.multiplier
-dry_day_frac = snakemake.params.dry_day_fraction
+
+# probabilities from dry to wet and wet to dry
+p_01 = snakemake.params.p_01
+p_10 = snakemake.params.p_10
+thres = snakemake.params.rainfall_thres
+
+# dry_day_frac = snakemake.params.dry_day_fraction
 jpg_out = os.path.join(os.path.split(fn)[0], "weather_plot.jpg")
 
 # now we make some random data, by making some random number between
 dates = pd.date_range(start_date, end_date)
 
 print(f"Creating weather data from {start_date} to {end_date}")
-# now we create a nice short random precipitation series. Put this into a pandas dataframe and write to a csv file
-precip = np.maximum((np.random.rand(len(dates)) - dry_day_frac) * multiplier, 0)
+# now we create a nice precipitation series. Put this into a pandas dataframe and write to a csv file
+precip = [0.]
+for n in range(len(dates)-1):
+    precip.append(
+        get_markov_rainfall(
+            precip[-1],
+            p_01,
+            p_10,
+            thres=thres,
+            multiplier=multiplier
+        )
+    )
 pot_evapo = np.ones(len(dates)) * evapo
 df = pd.DataFrame({"p": precip, "ep": pot_evapo}, index=dates)
 df.index.name = "date"
